f68e34616ab0f7adb0dd189ebb5ddf16ac86e015
David Vedvick <dvedvick@gmail.com>
2019-11-26 08:01:00 -0600

    Support more complex markdown

--- app/processing/ProjectTextProcessor.ts
+++ app/processing/ProjectTextProcessor.ts
@@ -1,20 +1,19 @@
 import unified from 'unified';
 import markdown from 'remark-parse';
-import toString from 'mdast-util-to-string';
 import stringify from 'remark-stringify'
 
 import IProcessProjectText from "./IProcessProjectText";
 import vfile from 'vfile';
 import { Parent } from 'unist';
 
-const markdownProcessor = unified().use(markdown, {commonmark: true}).use(stringify);
+const markdownProcessor = unified().use(markdown, {gfm: true}).use(stringify);
 
 export default class ProjectTextProcessor implements IProcessProjectText {
     async promiseProjectText(text: string): Promise<import("../Portfolio").default> {
         const markdown = markdownProcessor.parse(vfile(text)) as Parent;
 
-        const headings = markdown.children.filter(k => k.type == "heading");
-        const headline = headings.length > 0 ? toString(headings[0]) : null;
+        const headings = markdown.children.filter(k => k.type == "heading" && (<any>k).depth == 1);
+        const headline = headings.length > 0 ? markdownProcessor.stringify(headings[0]) : null;
 
         const paragraphs = markdown.children.filter(c => c.type == "paragraph");
         const body = paragraphs.length > 0 ? markdownProcessor.stringify(paragraphs[0]) : null;
--- app/processing/specs/given_text/when_processing_the_project.ts
+++ app/processing/specs/given_text/when_processing_the_project.ts
@@ -4,24 +4,47 @@ require('chai').should();
 
 describe("given text", () => {
     describe("when processing the project", () => {
+        const heading = `    My *Fake* Project`
+
+        const body =
+`Here's the **body** copy
+
+- Here's a bulleted list
+- With another bullet item
+
+## And here's a subsection
+
+### Another
+
+#### 4
+##### 5
+###### 6
+
+And some more text, *la-di-da-di-da*`
+
         const projectTestProcessor = new ProjectTextProcessor();
         const projectTask = projectTestProcessor.promiseProjectText(
-            "#    My Fake Project" +
-            "\n" +
-            "\n" +
-            "Here's the **body** copy" +
-            "\n");
+`
+## A sub-heading first, what?
+
+# ${heading}
+
+
+${body}
+
+
+# Contributing`);
 
         it("should have the right headline", async () => {
             const project = await projectTask;
 
-            project.headline.should.equal("My Fake Project");
+            project.headline.should.equal(heading);
         });
 
         it("should have the right body", async () => {
             const project = await projectTask;
             
-            project.body.should.equal("Here's the **body** copy");
+            project.body.should.equal(body);
         });
     });
 });
