3ceda81a8a1db2777eca5fe5d573762edca132ec
David Vedvick <dvedvick@gmail.com>
2020-01-12 09:24:15 -0600

    Support linux paths

--- app/supply/ReadmeFileTextFeed.ts
+++ app/supply/ReadmeFileTextFeed.ts
@@ -2,6 +2,8 @@ import ISupplyProjectText from "./ISupplyProjectText";
 import IGetDirectoryFiles from "../resources/IGetDirectoryFiles";
 import IReadFiles from "../resources/IReadFiles";
 
+const splitRegex = new RegExp(/[\\/]/, "g");
+
 export default class ReadmeFileTextFeed implements ISupplyProjectText {
 	constructor(private readonly directoryFiles: IGetDirectoryFiles, private readonly fileText: IReadFiles) {}
 
@@ -13,7 +15,7 @@ export default class ReadmeFileTextFeed implements ISupplyProjectText {
 	private async eventuallyGetReadmeText(repository: string): Promise<string> {
 		const files = await this.directoryFiles.promiseDirectoryFiles(repository);
 		const readMeFile = files.find(f => {
-			const segments = f.split("\\");
+			const segments = f.split(splitRegex);
 			const lastSegment = segments[segments.length - 1];
 			return lastSegment.toLowerCase() == "readme.md";
 		});
