949e9e80d689daebe19b3ba49e5037d7ec533757
David Vedvick <dvedvick@gmail.com>
2020-01-16 15:21:18 -0600

    Initial build

---
+++ app/Index.ts
@@ -0,0 +1,14 @@
+import Portfolio from "./Portfolio";
+import PortfolioCreator from "./PortfolioCreator";
+import ReadmeFileTextFeed from "./supply/ReadmeFileTextFeed";
+import DirectoryReader from "./resources/DirectoryReader";
+import FileReader from "./resources/FileReader";
+import ProjectTextProcessor from "./processing/ProjectTextProcessor";
+
+const portfolioCreator = new PortfolioCreator(
+	new ReadmeFileTextFeed(new DirectoryReader(), new FileReader()),
+	new ProjectTextProcessor());
+
+export default function(projects: string[]): Promise<Portfolio[]> {
+	return portfolioCreator.promisePortfolios(projects);
+}
